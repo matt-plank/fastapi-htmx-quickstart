c5defb59de4eed89b3e7a634d6e63185daae30ee
matt-plank <matthew_plank@outlook.com>
2025-01-01 18:53:27 +0000

    Added database lifecycle to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,15 @@ from typing import Generator
 from fastapi.testclient import TestClient
 from pytest import fixture
 
+from app import database
 from app.app import app
+from app.database.models import Base
 
 
 @fixture
 def client() -> Generator[TestClient, None, None]:
     with TestClient(app) as c:
+        assert database.engine is not None
+        Base.metadata.create_all(bind=database.engine)
         yield c
+        Base.metadata.drop_all(bind=database.engine)
