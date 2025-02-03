2a84a93921d6e5fbd94f5f5bfa665a4add73e2a9
matt-plank <matthew_plank@outlook.com>
2025-02-03 14:34:03 +0000

    Fixed test script

--- scripts/docker-compose-test.sh
+++ scripts/docker-compose-test.sh
@@ -7,6 +7,9 @@ docker compose -p $PROJECT_NAME -f docker-compose.test.yaml down
 
 echo "Running tests..."
 docker compose -p $PROJECT_NAME -f docker-compose.test.yaml $COMMAND
+EXIT_CODE=$?
 
 echo "Clearing test containers..."
 docker compose -p $PROJECT_NAME -f docker-compose.test.yaml down
+
+exit $EXIT_CODE
